e5fc179a34be70929272e1cc3f176014581be023
Anchal Sinha <anchalsinha@ucla.edu>
2021-12-13 23:10:49 -0800

    Added test cases

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tests import *
 
 if __name__ == '__main__':
     test_single_input()
+    test_single_input_rectangular_horizontal()
+    test_single_input_rectangular_vertical()
     test_double_input_same_weights()
     test_double_input_different_weights()
     test_double_input_different_weight_different_size_larger()
--- tests.py
+++ tests.py
@@ -42,6 +42,56 @@ def test_single_input():
 
     assert np.array_equal(result, ground_truth)
 
+def test_single_input_rectangular_horizontal():
+    inputMatrix = np.random.randint(1, 10, (MMU_ROWS//3, MMU_COLS))
+    weights = np.random.randint(1, 100, (MMU_ROWS, MMU_COLS//2))
+    output_shape = [inputMatrix.shape[0], weights.shape[1]]
+
+    ub, acc, mmu, wf = createTPU([inputMatrix])
+
+    ub.store_input(inputMatrix)
+    wf.add_weights(weights)
+
+    cycles = 2*inputMatrix.shape[0] + inputMatrix.shape[1] - 1 # 2*rows + cols - 1
+
+    ub.allocate_output(output_shape)
+
+    for i in range(cycles):
+        wf.cycle()
+        mmu.cycle()
+    ub.store_acc(acc, output_shape)
+
+    ground_truth = np.matmul(inputMatrix, weights)
+    result = ub.sram_outputs[0]
+    assert np.array_equal(result, ground_truth)
+
+
+def test_single_input_rectangular_vertical():
+    inputMatrix = np.random.randint(1, 10, (MMU_ROWS, MMU_COLS//3))
+    weights = np.random.randint(1, 100, (MMU_ROWS//3, MMU_COLS//2))
+    output_shape = [inputMatrix.shape[0], weights.shape[1]]
+
+    ub, acc, mmu, wf = createTPU([inputMatrix])
+
+    ub.store_input(inputMatrix)
+    wf.add_weights(weights)
+
+    cycles = 2*output_shape[0] + output_shape[1] - 1 # 2*rows + cols - 1
+
+    ub.allocate_output(output_shape)
+
+    for i in range(cycles):
+        wf.cycle()
+        mmu.cycle()
+        acc.display()
+    ub.store_acc(acc, output_shape)
+
+    ground_truth = np.matmul(inputMatrix, weights)
+    result = ub.sram_outputs[0]
+    print(ground_truth)
+    print(result)
+    assert np.array_equal(result, ground_truth)
+
 def test_double_input_same_weights():
     inputMatrix1 = np.random.randint(1, 10, (MMU_ROWS, MMU_COLS))
     inputMatrix2 = np.random.randint(1, 10, (MMU_ROWS, MMU_COLS))
@@ -56,8 +106,8 @@ def test_double_input_same_weights():
     wf.add_weights(weights)
     wf.add_weights(weights)
 
-    cycles1 = 2*inputMatrix1.shape[0] + inputMatrix1.shape[1] - 1 # 2*rows + cols - 1
-    cycles2 = 2*inputMatrix2.shape[0] - 1
+    cycles1 = 2*output_shapes[0][0] + output_shapes[0][1] - 1 # 2*rows + cols - 1
+    cycles2 = 2*output_shapes[1][0] - 1
 
     ub.allocate_output(output_shapes[0])
     ub.allocate_output(output_shapes[1])
